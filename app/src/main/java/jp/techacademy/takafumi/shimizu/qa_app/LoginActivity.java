4a370698efef591f2e31bd8bd2bb0f2b6364e8cd
Takafumi Shimizu <tkf.smz@gmail.com>
2017-12-10 19:36:58 +0900

    アカウントが作成できない

--- app/src/main/java/jp/techacademy/takafumi/shimizu/qa_app/LoginActivity.java
+++ app/src/main/java/jp/techacademy/takafumi/shimizu/qa_app/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -70,6 +71,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     // プログレスダイアログを非表示にする
                     mProgress.dismiss();
+
                 }
             }
         };
